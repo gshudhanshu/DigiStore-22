a667d7949461c9327caa6a2af6b3f1a03292d469
Shudhanshu Gunjal <33764308+gshudhanshu@users.noreply.github.com>
2022-06-14 02:53:30 +0530

    meta tag replaced

--- frontend/src/pages/home/HomeFashion.js
+++ frontend/src/pages/home/HomeFashion.js
@@ -1,39 +1,39 @@
-import React, { Fragment } from "react";
-import MetaTags from "react-meta-tags";
-import LayoutOne from "../../layouts/LayoutOne";
-import HeroSliderOne from "../../wrappers/hero-slider/HeroSliderOne";
-import FeatureIcon from "../../wrappers/feature-icon/FeatureIcon";
-import TabProduct from "../../wrappers/product/TabProduct";
-import BlogFeatured from "../../wrappers/blog-featured/BlogFeatured";
+import React, { Fragment } from 'react'
+import { Helmet } from 'react-helmet'
+import LayoutOne from '../../layouts/LayoutOne'
+import HeroSliderOne from '../../wrappers/hero-slider/HeroSliderOne'
+import FeatureIcon from '../../wrappers/feature-icon/FeatureIcon'
+import TabProduct from '../../wrappers/product/TabProduct'
+import BlogFeatured from '../../wrappers/blog-featured/BlogFeatured'
 
 const HomeFashion = () => {
   return (
     <Fragment>
-      <MetaTags>
+      <Helmet>
         <title>Flone | Fashion Home</title>
         <meta
-          name="description"
-          content="Fashion home of flone react minimalist eCommerce template."
+          name='description'
+          content='Fashion home of flone react minimalist eCommerce template.'
         />
-      </MetaTags>
+      </Helmet>
       <LayoutOne
-        headerContainerClass="container-fluid"
-        headerPaddingClass="header-padding-1"
+        headerContainerClass='container-fluid'
+        headerPaddingClass='header-padding-1'
       >
         {/* hero slider */}
         <HeroSliderOne />
 
         {/* featured icon */}
-        <FeatureIcon spaceTopClass="pt-100" spaceBottomClass="pb-60" />
+        <FeatureIcon spaceTopClass='pt-100' spaceBottomClass='pb-60' />
 
         {/* tab product */}
-        <TabProduct spaceBottomClass="pb-60" category="fashion" />
+        <TabProduct spaceBottomClass='pb-60' category='fashion' />
 
         {/* blog featured */}
-        <BlogFeatured spaceBottomClass="pb-55" />
+        <BlogFeatured spaceBottomClass='pb-55' />
       </LayoutOne>
     </Fragment>
-  );
-};
+  )
+}
 
-export default HomeFashion;
+export default HomeFashion
