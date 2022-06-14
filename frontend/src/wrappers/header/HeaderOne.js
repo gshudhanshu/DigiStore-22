1a1222c6c99df1c50bfbcd0ad83bdb9176d07758
Shudhanshu Gunjal <33764308+gshudhanshu@users.noreply.github.com>
2022-06-14 19:27:34 +0530

    cart bug

--- frontend/src/wrappers/header/HeaderOne.js
+++ frontend/src/wrappers/header/HeaderOne.js
@@ -1,10 +1,10 @@
-import PropTypes from "prop-types";
-import React, { useEffect, useState } from "react";
-import Logo from "../../components/header/Logo";
-import NavMenu from "../../components/header/NavMenu";
-import IconGroup from "../../components/header/IconGroup";
-import MobileMenu from "../../components/header/MobileMenu";
-import HeaderTop from "../../components/header/HeaderTop";
+import PropTypes from 'prop-types'
+import React, { useEffect, useState } from 'react'
+import Logo from '../../components/header/Logo'
+import NavMenu from '../../components/header/NavMenu'
+import IconGroup from '../../components/header/IconGroup'
+import MobileMenu from '../../components/header/MobileMenu'
+import HeaderTop from '../../components/header/HeaderTop'
 
 const HeaderOne = ({
   layout,
@@ -12,38 +12,38 @@ const HeaderOne = ({
   borderStyle,
   headerPaddingClass,
   headerPositionClass,
-  headerBgClass
+  headerBgClass,
 }) => {
-  const [scroll, setScroll] = useState(0);
-  const [headerTop, setHeaderTop] = useState(0);
+  const [scroll, setScroll] = useState(0)
+  const [headerTop, setHeaderTop] = useState(0)
 
   useEffect(() => {
-    const header = document.querySelector(".sticky-bar");
-    setHeaderTop(header.offsetTop);
-    window.addEventListener("scroll", handleScroll);
+    const header = document.querySelector('.sticky-bar')
+    setHeaderTop(header.offsetTop)
+    window.addEventListener('scroll', handleScroll)
     return () => {
-      window.removeEventListener("scroll", handleScroll);
-    };
-  }, []);
+      window.removeEventListener('scroll', handleScroll)
+    }
+  }, [])
 
   const handleScroll = () => {
-    setScroll(window.scrollY);
-  };
+    setScroll(window.scrollY)
+  }
 
   return (
     <header
-      className={`header-area clearfix ${headerBgClass ? headerBgClass : ""} ${
-        headerPositionClass ? headerPositionClass : ""
+      className={`header-area clearfix ${headerBgClass ? headerBgClass : ''} ${
+        headerPositionClass ? headerPositionClass : ''
       }`}
     >
       <div
-        className={`${headerPaddingClass ? headerPaddingClass : ""} ${
-          top === "visible" ? "d-none d-lg-block" : "d-none"
+        className={`${headerPaddingClass ? headerPaddingClass : ''} ${
+          top === 'visible' ? 'd-none d-lg-block' : 'd-none'
         } header-top-area ${
-          borderStyle === "fluid-border" ? "border-none" : ""
+          borderStyle === 'fluid-border' ? 'border-none' : ''
         }`}
       >
-        <div className={layout === "container-fluid" ? layout : "container"}>
+        <div className={layout === 'container-fluid' ? layout : 'container'}>
           {/* header top */}
           <HeaderTop borderStyle={borderStyle} />
         </div>
@@ -51,22 +51,22 @@ const HeaderOne = ({
 
       <div
         className={` ${
-          headerPaddingClass ? headerPaddingClass : ""
+          headerPaddingClass ? headerPaddingClass : ''
         } sticky-bar header-res-padding clearfix ${
-          scroll > headerTop ? "stick" : ""
+          scroll > headerTop ? 'stick' : ''
         }`}
       >
-        <div className={layout === "container-fluid" ? layout : "container"}>
-          <div className="row">
-            <div className="col-xl-2 col-lg-2 col-md-6 col-4">
+        <div className={layout === 'container-fluid' ? layout : 'container'}>
+          <div className='row'>
+            <div className='col-xl-2 col-lg-2 col-md-6 col-4'>
               {/* header logo */}
-              <Logo imageUrl="/assets/img/logo/logo.png" logoClass="logo" />
+              <Logo imageUrl='/assets/img/logo/logo.png' logoClass='logo' />
             </div>
-            <div className="col-xl-8 col-lg-8 d-none d-lg-block">
+            <div className='col-xl-8 col-lg-8 d-none d-lg-block'>
               {/* Nav menu */}
               <NavMenu />
             </div>
-            <div className="col-xl-2 col-lg-2 col-md-6 col-8">
+            <div className='col-xl-2 col-lg-2 col-md-6 col-8'>
               {/* Icon group */}
               <IconGroup />
             </div>
@@ -76,15 +76,15 @@ const HeaderOne = ({
         <MobileMenu />
       </div>
     </header>
-  );
-};
+  )
+}
 
 HeaderOne.propTypes = {
   borderStyle: PropTypes.string,
   headerPaddingClass: PropTypes.string,
   headerPositionClass: PropTypes.string,
   layout: PropTypes.string,
-  top: PropTypes.string
-};
+  top: PropTypes.string,
+}
 
-export default HeaderOne;
+export default HeaderOne
