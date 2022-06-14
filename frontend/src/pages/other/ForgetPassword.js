97a1963872efec85d2dcd8500e09d718f8ef1a60
Shudhanshu Gunjal <33764308+gshudhanshu@users.noreply.github.com>
2022-06-14 19:48:12 +0530

    Revert "separate forms"
    This reverts commit 697e1f255d83adff4069ee6ec57d3410739ae73d.

--- frontend/src/pages/other/ForgetPassword.js
+++ frontend/src/pages/other/ForgetPassword.js
@@ -11,13 +11,13 @@ import Breadcrumb from '../../wrappers/breadcrumb/Breadcrumb'
 import PhoneInput from 'react-phone-input-2'
 import 'react-phone-input-2/lib/style.css'
 
-const ForgetPassword = ({ location }) => {
+const LoginRegister = ({ location }) => {
   const { pathname } = location
 
   return (
     <Fragment>
       <Helmet>
-        <title>DigiStore | Forget Password</title>
+        <title>DigiStore | Login</title>
         <meta
           name='description'
           content='Compare page of flone react minimalist eCommerce template.'
@@ -25,7 +25,7 @@ const ForgetPassword = ({ location }) => {
       </Helmet>
       <BreadcrumbsItem to={process.env.PUBLIC_URL + '/'}>Home</BreadcrumbsItem>
       <BreadcrumbsItem to={process.env.PUBLIC_URL + pathname}>
-        Forget Password
+        Login Register
       </BreadcrumbsItem>
       <LayoutOne headerTop='visible'>
         {/* breadcrumb */}
@@ -35,48 +35,130 @@ const ForgetPassword = ({ location }) => {
             <div className='row'>
               <div className='col-lg-7 col-md-12 ml-auto mr-auto'>
                 <div className='login-register-wrapper'>
-                  <Nav variant='pills' className='login-register-tab-list'>
-                    <Nav.Item>
-                      <Nav.Link eventKey='forget'>
-                        <h4>Forget Password</h4>
-                      </Nav.Link>
-                    </Nav.Item>
-                  </Nav>
-                  <div className='login-form-container'>
-                    <div className='login-register-form'>
-                      <form>
-                        <label>Mobile number</label>
-                        <PhoneInput
-                          country={'bb'}
-                          onlyCountries={['bb', 'in']}
-                          placeholder='+1246 530 3444'
-                          name='mobile-number'
-                          // id='mobile-number-field'
-                          // value={this.state.phone}
-                          // onChange={(phone) => this.setState({ phone })}
-                        />
-                        <label htmlFor='user-password-field'>Password</label>
-                        <input
-                          type='password'
-                          name='user-password'
-                          id='user-password-field'
-                          placeholder='Password'
-                        />
-                        <div className='button-box'>
-                          <div className='login-toggle-btn'>
-                            <input type='checkbox' />
-                            <label className='ml-10'>Remember me</label>
-                            <Link to={process.env.PUBLIC_URL + '/login'}>
-                              Already have an account?
-                            </Link>
+                  <Tab.Container defaultActiveKey='login'>
+                    <Nav variant='pills' className='login-register-tab-list'>
+                      <Nav.Item>
+                        <Nav.Link eventKey='login'>
+                          <h4>Login</h4>
+                        </Nav.Link>
+                      </Nav.Item>
+                      <Nav.Item>
+                        <Nav.Link eventKey='register'>
+                          <h4>Register</h4>
+                        </Nav.Link>
+                      </Nav.Item>
+                    </Nav>
+                    <Tab.Content>
+                      <Tab.Pane eventKey='login'>
+                        <div className='login-form-container'>
+                          <div className='login-register-form'>
+                            <form>
+                              <label>Mobile number</label>
+                              <PhoneInput
+                                country={'bb'}
+                                onlyCountries={['bb', 'in']}
+                                placeholder='+1246 530 3444'
+                                name='mobile-number'
+                                // id='mobile-number-field'
+                                // value={this.state.phone}
+                                // onChange={(phone) => this.setState({ phone })}
+                              />
+                              <label htmlFor='user-password-field'>
+                                Password
+                              </label>
+                              <input
+                                type='password'
+                                name='user-password'
+                                id='user-password-field'
+                                placeholder='Password'
+                              />
+                              <div className='button-box'>
+                                <div className='login-toggle-btn'>
+                                  <input type='checkbox' />
+                                  <label className='ml-10'>Remember me</label>
+                                  <Link to={process.env.PUBLIC_URL + '/'}>
+                                    Forgot Password?
+                                  </Link>
+                                </div>
+                                <button type='submit'>
+                                  <span>Login</span>
+                                </button>
+                              </div>
+                            </form>
                           </div>
-                          <button type='submit'>
-                            <span>Login</span>
-                          </button>
                         </div>
-                      </form>
-                    </div>
-                  </div>
+                      </Tab.Pane>
+                      <Tab.Pane eventKey='register'>
+                        <div className='login-form-container'>
+                          <div className='login-register-form'>
+                            <form>
+                              <label htmlFor='first-name-field'>
+                                First name
+                              </label>
+                              <input
+                                type='text'
+                                name='first-name'
+                                placeholder='First Name'
+                                id='first-name-field'
+                              />
+                              <label htmlFor='last-name-field'>Last name</label>
+                              <input
+                                type='text'
+                                name='last-name'
+                                id='last-name-field'
+                                placeholder='Last Name'
+                              />
+                              <label>Mobile number</label>
+                              <PhoneInput
+                                country={'bb'}
+                                onlyCountries={['bb', 'in']}
+                                placeholder='+1246 530 3444'
+                                name='mobile-number'
+                                id='mobile-number-field'
+                                // value={this.state.phone}
+                                // onChange={(phone) => this.setState({ phone })}
+                              />
+
+                              <div className='button-box mb-4'>
+                                <button type='submit'>
+                                  <span>Send SMS Code</span>
+                                </button>
+                                <span className='ml-3'>
+                                  Resend after 60 sec
+                                </span>
+                              </div>
+
+                              <label htmlFor='mobile-otp-field'>
+                                Received SMS Code
+                              </label>
+                              <input
+                                type='number'
+                                name='mobile-otp'
+                                id='mobile-otp-field'
+                                placeholder='123456'
+                              />
+
+                              <label htmlFor='register-password-field'>
+                                Password
+                              </label>
+                              <input
+                                type='password'
+                                name='register-password'
+                                id='register-password-field'
+                                placeholder='Password'
+                              />
+
+                              <div className='button-box'>
+                                <button type='submit'>
+                                  <span>Register</span>
+                                </button>
+                              </div>
+                            </form>
+                          </div>
+                        </div>
+                      </Tab.Pane>
+                    </Tab.Content>
+                  </Tab.Container>
                 </div>
               </div>
             </div>
@@ -87,8 +169,8 @@ const ForgetPassword = ({ location }) => {
   )
 }
 
-ForgetPassword.propTypes = {
+LoginRegister.propTypes = {
   location: PropTypes.object,
 }
 
-export default ForgetPassword
+export default LoginRegister
